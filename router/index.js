451080c05dfc78d904ac716a389aa3f5afd9da80
Lærke Nielsen <118178518+Laerke-Nicole@users.noreply.github.com>
2023-04-21 17:50:55 +0200

    Add files via upload

---
+++ router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue')
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/PortfolioView.vue')
+    },
+    {
+      path: '/portfoliodetails/:id',
+      name: 'portfoliodetails',
+      component: () => import('../views/PortfolioDetail.vue'),
+      props:true
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+  ]
+})
+
+export default router
